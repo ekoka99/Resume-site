5628fff5e0b810813c6129733ea615a66ca6f74d
Elio Koka <elio.koka@gmail.com>
2024-08-14 09:29:09 -0400

    url change 3

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -57,7 +57,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: 'https://drive.google.com/file/d/1y4EvAsm_VXLM3XvhxGPcdZxgWhffwv5Y/view?usp=sharing',
+      href: 'https://drive.google.com/uc?export=download&id=1y4EvAsm_VXLM3XvhxGPcdZxgWhffwv5Y',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
