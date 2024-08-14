d4aade00b1bb1a448d00ea36f0fa877cb0abd709
ekoka99 <52168843+ekoka99@users.noreply.github.com>
2024-08-14 08:36:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /* eslint-env node */
-
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
+  output: 'export',
+  assetPrefix: '/Resume-site/',  // Replace 'Resume-site' with your actual repository name
+  basePath: '/Resume-site',      // Replace 'Resume-site' with your actual repository name
+
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
-
     // Next 12 has multiple TS loaders, and we need to update all of them.
     const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
-
     tsRules.forEach(rule => {
       // eslint-disable-next-line no-param-reassign
       rule.include = undefined;
     });
-
     return config;
   },
   compress: true,
@@ -24,6 +24,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
+    unoptimized: true,  // This is important for static export
     remotePatterns: [
       {
         protocol: 'https',
