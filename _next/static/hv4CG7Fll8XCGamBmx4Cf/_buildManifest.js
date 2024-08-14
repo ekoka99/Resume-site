4235dcd49e7700e9d85ee1bcbdd8daec9d3c2a9d
Elio Koka <elio.koka@gmail.com>
2024-08-14 09:34:21 -0400

    Updates

---
+++ _next/static/hv4CG7Fll8XCGamBmx4Cf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/721-d95b3aadfb30fc41.js","static/chunks/pages/index-cd47ec5830522dd1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
