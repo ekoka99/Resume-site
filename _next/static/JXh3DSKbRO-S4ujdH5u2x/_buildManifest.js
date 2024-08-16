e4a7b3b8dad131addb4343eceec92803f270c69c
ekoka99 <ekoka99@users.noreply.github.com>
2024-08-16 12:55:44 +0000

    deploy: 5b7deb2ab4cc42c4e2f9c31dc80448bc566888f5

---
+++ _next/static/JXh3DSKbRO-S4ujdH5u2x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/721-d95b3aadfb30fc41.js","static/chunks/pages/index-7c7f00c51f35e127.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
